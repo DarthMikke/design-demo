0d876d3217ba562c5ac25ad385ce34a8afb6660b
Michal Jan Warecki <mwarecki1@gmail.com>
2023-09-29 08:53:05 +0200

    Dropdown in the counter view.

--- src/routes/counter/View.tsx
+++ src/routes/counter/View.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Pane from "../../components/Pane";
+import Dropdown from "../../components/Dropdown";
 
 export default function() {
   const [counter, setCounter] = useState(5);
@@ -9,6 +10,10 @@ export default function() {
     </p>
     <button className={"button primary"} onClick={() => {setCounter(counter + 1)}}>Increase by 1</button>
     <button className={"button destructive"} onClick={() => {setCounter(counter - 1)}}>Decrease by 1</button>
-    <button className={"button secondary"}>Just a button.</button>
+    <Dropdown type={"secondary"} title={"Other options"}>
+      <button onClick={() => {setCounter(5)}}>Reset</button>
+      <button onClick={() => {setCounter(counter + 10)}}>Increase by 10</button>
+      <button onClick={() => {setCounter(counter - 10)}}>Decrease by 10</button>
+    </Dropdown>
   </Pane>
 }
