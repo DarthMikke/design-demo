350c582852c73e3bb9b985160313d4fa879dfb13
Michal Jan Warecki <mwarecki1@gmail.com>
2023-09-29 08:48:43 +0200

    Buttons in the counter view

--- src/routes/counter/View.tsx
+++ src/routes/counter/View.tsx
@@ -7,6 +7,8 @@ export default function() {
     <p>
       Counter: {counter}
     </p>
-    <button onClick={() => {setCounter(counter + 1)}}>Increase by 1</button>
+    <button className={"button primary"} onClick={() => {setCounter(counter + 1)}}>Increase by 1</button>
+    <button className={"button destructive"} onClick={() => {setCounter(counter - 1)}}>Decrease by 1</button>
+    <button className={"button secondary"}>Just a button.</button>
   </Pane>
 }
