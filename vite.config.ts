90bfcc3ffd1b32fc7920cc4d6a7765952b74cb8b
Michal Jan Warecki <mwarecki1@gmail.com>
2023-10-07 20:06:06 +0200

    Config for a bundled CSS.

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        // demoApp: resolve(__dirname, 'index.html')
+        demoApp: 'index.html',
+        bundled: 'src/bundle.scss',
+      },
+      output: {
+        dir: "public",
+        assetFileNames: "assets/[name].[ext]",
+      }
+    }
+  }
 })
